1d854f1e6175be8d6344c166e95d47dc17bbc4cd
dmontecino <dmontecino@ucdavis.edu>
2021-03-15 23:38:53 -0700

    add instructions to the app and deleting extra commmands

--- app_to_assess_data.R
+++ app_to_assess_data.R
@@ -1,3 +1,5 @@
+library(shiny)
+
 ui <- fluidPage( # lets. check 
   sidebarLayout(
     sidebarPanel(
@@ -18,7 +20,13 @@ ui <- fluidPage( # lets. check
     ),
     mainPanel(
       tableOutput("contents")
-    )
+    ),
+    
+    mainPanel("Download all the data from WHIP. Specifically, go to the quick search and click on the down arrow.
+              In 'Category', select 'Event', and in 'Field' select 'Event Code'. Type 'wcs' in 'Value' and click on 
+              the green button on the upper-right corner. Click on 'Export' and save the excel file. Finally,
+              upload the excel file to the app, click on 'Assess...' and wait for 3-5 minutes. Download the .html
+              output and presto! (Let me know if you have any problems)")
   )
 )
 
@@ -27,14 +35,6 @@ server <- function(input, output) {
     # For PDF output, change this to "report.pdf"
     filename = "data_quality.html",
     content = function(file) {
-      
-      # report<-"Checking_quality_whip_data_per_country.Rmd"
-      # src <- normalizePath(report)
-      # 
-      # owd <- setwd(tempdir())
-      # on.exit(setwd(owd))
-      # file.copy(src, report, overwrite = TRUE)
-      # 
       
       
       # Copy the report file to a temporary directory before processing it, in
