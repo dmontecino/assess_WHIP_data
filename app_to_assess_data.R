37914b96effd3804117c54f0aa806a36843c83c0
dmontecino <dmontecino@ucdavis.edu>
2022-10-17 21:56:31 -0300

    adapting the scripts for the server app

--- app_to_assess_data.R
+++ app_to_assess_data.R
@@ -7,9 +7,9 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       fileInput("datain", "Upload WHIP Export", buttonLabel = "Choose file..."),
-      radioButtons('country', 'Select country', 
-                   choiceValues=c("Cambodia", "Lao PDR", "Vietnam"),
-                   choiceNames = c('Cambodia', 'Laos', 'Viet nam'), inline = F),
+      # radioButtons('country', 'Select country',
+      #              choiceValues=c("Cambodia", "Lao PDR", "Vietnam"),
+      #              choiceNames = c('Cambodia', 'Laos', 'Viet nam'), inline = F),
       downloadButton("report", "Assess data")
     ),
     
@@ -17,7 +17,7 @@ ui <- fluidPage(
     mainPanel("Download all the data from WHIP. Specifically, go to the quick search and click on the down arrow.
               In 'Category', select 'Event', and in 'Field' select 'Event Code'. Type 'wcs' in 'Value' and click on 
               the green button on the upper-right corner. 
-              Select all the sections in the export options and select 'Specimens' and 'Observation' as well. 
+              Select all the sections in the export options and select 'Observation', 'Specimen', and 'Environmental Specimen'. 
               Click on 'Export' and save the excel file. 
               Upload the excel file to the app, click on 'Assess data' and wait for some minutes (seriouly, just wait). 
               Open and download the .html output (Let me know if you have any problems).")
@@ -25,24 +25,38 @@ ui <- fluidPage(
 )
 
 
-
+options(shiny.maxRequestSize=30*1024^2)
 
 server <- function(input, output) {
   
+  # options(shiny.maxRequestSize=30*1024^2) 
   
   # first open the observation and spec to get the number of columns
   
    dataset<-reactive({
     # inFile <- input$datain
-    dat<-read_excel(path =input$datain$datapath, sheet = 1, col_types = "text")
-    return(dat)
+    dat1<-read_excel(path =input$datain$datapath, sheet = 1, col_types = "text")
+    return(dat1)
   })
   
+   # dataset<-reactive({
+   #   # inFile <- input$datain
+   #   dat<-lapply(c(1:3), function(x) read_excel(path =input$datain$datapath, sheet = x, col_types = "text"))
+   #   return(dat)
+   # })
+     
   dataset2<-reactive({
     # inFile <- input$datain
-    dat<-read_excel(path =input$datain$datapath, sheet = 2, col_types = "text")
-    return(dat)
+    dat2<-read_excel(path =input$datain$datapath, sheet = 2, col_types = "text")
+    return(dat2)
   })
+  
+  dataset3<-reactive({
+    # inFile <- input$datain
+    dat3<-read_excel(path =input$datain$datapath, sheet = 3, col_types = "text")
+    return(dat3)
+  })
+  
   
   output$report <- downloadHandler(
     filename = "report.html",
@@ -54,12 +68,15 @@ server <- function(input, output) {
       on.exit(setwd(owd))
       file.copy(src, report, overwrite = TRUE)
       
-      spec <- dataset()
-      obs <- dataset2()
+      obs.temp <- dataset()
+      spec.temp <- dataset2()
+      env_spec.temp <- dataset3()
       
-      params <- list(spec = spec,
-                     obs = obs,
-                     country.of.interest=input$country)
+      params <- list(obs = obs.temp,
+                     spec = spec.temp,
+                     env_spec = env_spec.temp
+                     #country.of.interest=input$country
+      )
       
       out<-rmarkdown::render(report,
                              output_file = file,
